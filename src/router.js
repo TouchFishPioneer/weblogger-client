fb8275033033b49807d794777ba3970b15f54a58
wurahara <wurahara@126.com>
2018-12-11 13:12:46 +0400

    rewrite the webpage using Bootstrap 4

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Enter from './views/Index.vue'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'index',
+      component: Enter
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
     },
