6534f8069202073e3c7de9b766978e18ad102bcb
wurahara <wurahara@126.com>
2018-12-12 07:47:17 +0400

    basically finish the view layer

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="modal fade" id="overModal" tabindex="-1" role="dialog" aria-labelledby="overModalLabel">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <h4 class="modal-title" id="overModalLabel">恭喜！</h4>
-          </div>
-          <div class="modal-body">
-            <p>您已成功录入所有样本！感谢您的支持！</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">好的</button>
-            <button type="button" class="btn btn-primary" id="enter-again-btn">再次录入</button>
-          </div>
-        </div>
+  <div class="container">
+    <div class="col-xs-12">
+      <br>
+
+      <b-alert show variant="success">
+        <h4 class="alert-heading">Well done!</h4>
+        <p>You have completed all the work required, thank you again for your cooperation.</p>
+        <hr>
+        <p class="mb-0">You can exit this application by clicking the
+          <strong>Exit</strong> button or by clicking the
+          <strong>Re-enter</strong> button to start another round of input.
+        </p>
+      </b-alert>
+
+      <br>
+
+      <div>
+        <b-button variant="danger" style="margin-right: 10px">Exit</b-button>
+        <b-button variant="primary">Re-enter</b-button>
       </div>
     </div>
+  </div>
 </template>
